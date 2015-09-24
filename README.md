bb3751712fdb749b8ed41c2da268671709b966b9
mck <mick@semb.wever.org>
2015-09-25 04:49:55 +1000

    Enable Zipkin tracing based off the CASSANDRA-10392 patch.
    --
     Allow Cassandra to trace to custom tracing solutions
    
     - MessageOut shouldn't know of the specific trace headers that need to be passed over the message service,
         Tracing.getTraceHeaders() provides the headers now
     - pass in the customPayload to Tracing.newSession(..) allowing third-party trace headers to be piggybacked for fullstack tracing
     - TraceState.traceImpl(..) added for subclassing
     - the default cassandra tracing functionality that writes to system_* tables moved out to TracingImpl and TraceStateImpl classes
        Cassandra's general tracing behaviour remains in Tracing and TraceState, but it doesn't yet work to directly subclass these as other
    parts of Cassandra is still expecting to find a corresponding trace in the system_* table for this request (todo)
    
    ToDo
     - fix tracing in OutboundTcpConnection (needs to delegate to correct tracing impl)
     - fix ExpiredTraceState (needs to delegate to correct tracing impl)
     - zipkin tracing without cassandra's internal tracing (TracingImpl + TraceStateImpl)

---
+++ README.md
@@ -0,0 +1,6 @@
+A Zipkin tracing plugin for cassandra.
+
+ See CASSANDRA-10392 for the patch to extend Cassandra's tracing that this project plugs into.
+
+There is still a number of TODOs and FIXMEs in the project, and other things that should be more easily configured, along with a build 
+system, that are missing for now. For now the project is more a POC and reference for CASSANDRA-10392
---
+++ src/main/java/org/apache/cassandra/tracing/ZipkinTraceState.java
@@ -0,0 +1,108 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.cassandra.tracing;
+
+import com.github.kristofa.brave.Brave;
+import com.github.kristofa.brave.ClientTracer;
+import com.github.kristofa.brave.SpanCollector;
+import com.github.kristofa.brave.ServerSpan;
+import com.twitter.zipkin.gen.Annotation;
+import com.twitter.zipkin.gen.Span;
+import java.net.InetAddress;
+import java.util.Deque;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentLinkedDeque;
+import java.util.concurrent.TimeUnit;
+
+
+/**
+ * ThreadLocal state for a tracing session. The presence of an instance of this class as a ThreadLocal denotes that an
+ * operation is being traced.
+ */
+final class ZipkinTraceState extends TraceStateImpl
+{
+    private final ClientTracer clientTracer;
+    private final SpanCollector spanCollector;
+    private final ServerSpan serverSpan;
+
+    private final Deque<Span> openSpans = new ConcurrentLinkedDeque();
+    private final ThreadLocal<Span> previous = new ThreadLocal<>();
+
+    public ZipkinTraceState(
+            InetAddress coordinator,
+            UUID sessionId,
+            Tracing.TraceType traceType,
+            ServerSpan serverSpan,
+            ClientTracer clientTracer,
+            SpanCollector spanCollector)
+    {
+        super(coordinator, sessionId, traceType);
+        assert null != serverSpan;
+        this.serverSpan = serverSpan;
+        this.clientTracer = clientTracer;
+        this.spanCollector = spanCollector;
+    }
+
+    protected void traceImpl(String message)
+    {
+        traceImplWithSpans(message);
+    }
+
+    void close()
+    {
+        Brave.getServerSpanThreadBinder().setCurrentSpan(serverSpan);
+        for (Span span : openSpans)
+        {
+            Brave.getClientSpanThreadBinder().setCurrentSpan(span);
+            clientTracer.setClientReceived();
+        }
+        openSpans.clear();
+    }
+
+    private void traceImplWithSpans(String message)
+    {
+        long startTime = System.currentTimeMillis() * 1000 + (TimeUnit.NANOSECONDS.toMicros(System.nanoTime()) % 1000);
+
+        Brave.getServerSpanThreadBinder().setCurrentSpan(serverSpan);
+        if (null != previous.get())
+        {
+            clientTracer.setClientReceived();
+            openSpans.remove(previous.get());
+        }
+        clientTracer.startNewSpan(message + " [" + Thread.currentThread().getName() + "]");
+        clientTracer.setClientSent();
+        Span prev = Brave.getClientSpanThreadBinder().getCurrentClientSpan();
+        previous.set(prev);
+        openSpans.addLast(prev);
+        super.traceImpl(message);
+    }
+
+    private void traceImplUsingAnnotations(String message)
+    {
+        long startTime = System.currentTimeMillis() * 1000 + (TimeUnit.NANOSECONDS.toMicros(System.nanoTime()) % 1000);
+
+        Annotation annotation = new Annotation();
+        annotation.setTimestamp(startTime);
+        annotation.setValue(message);
+        synchronized (serverSpan.getSpan())
+        {
+            serverSpan.getSpan().addToAnnotations(annotation);
+        }
+        super.traceImpl(message);
+    }
+}
